05d1a7ee5c1d524bb6592fba807c1c88f5f8a61b
I-M-Marinov <lcfrrr@gmail.com>
2023-09-14 11:07:32 -0500

    P1.Basic Stack Operations
    Added solution for P1. Basic Stack Operations

--- P1.BasicStackOperations/Program.cs
+++ P1.BasicStackOperations/Program.cs
@@ -1,1 +1,38 @@
 ﻿
+int[] ruleNumbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+int pushElements = ruleNumbers[0];
+int popElements = ruleNumbers[1];
+int checkIfNumberIsPresent  = ruleNumbers[2];
+
+int[] inputNumbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+Stack<int> numbersStack = new Stack<int>(pushElements);
+
+foreach (var number in inputNumbers)
+{
+    numbersStack.Push(number);
+}
+
+for (int i = 0; i < popElements ; i++)
+{
+    numbersStack.Pop();
+}
+
+if (numbersStack.Contains(checkIfNumberIsPresent))
+{
+    Console.WriteLine("true");
+}
+else if(!numbersStack.Contains(checkIfNumberIsPresent))
+{
+    if (numbersStack.Count == 0)
+    {
+        Console.WriteLine(0);
+    }
+    else
+    {
+        int lowestNumber = numbersStack.Min();
+        Console.Write($"{lowestNumber}");
+    }
+
+    
+}
