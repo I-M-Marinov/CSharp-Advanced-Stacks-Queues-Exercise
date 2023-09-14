702eda61339d4abeb83413a54c6e57671bbf5368
I-M-Marinov <lcfrrr@gmail.com>
2023-09-14 11:26:47 -0500

    Added solution to P2.BasicQueueOperations
    Added solution to P2.Basic Queue Operations

--- P2.BasicQueueOperations/Program.cs
+++ P2.BasicQueueOperations/Program.cs
@@ -1,2 +1,33 @@
-﻿
+﻿int[] ruleNumbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+int[] inputNumbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
 
+int pushElements = ruleNumbers[0];
+int popElements = ruleNumbers[1];
+int checkIfNumberIsPresent = ruleNumbers[2];
+
+Queue<int> numbersStack = new Queue<int>(inputNumbers.Take(pushElements));
+
+
+for (int i = 0; i < popElements; i++)
+{
+    numbersStack.Dequeue();
+}
+
+if (numbersStack.Contains(checkIfNumberIsPresent))
+{
+    Console.WriteLine("true");
+}
+else if (!numbersStack.Contains(checkIfNumberIsPresent))
+{
+    if (numbersStack.Count == 0)
+    {
+        Console.WriteLine(0);
+    }
+    else
+    {
+        int lowestNumber = numbersStack.Min();
+        Console.Write($"{lowestNumber}");
+    }
+
+
+}
