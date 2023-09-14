0b603a15d0588ed4c1dc4281496983a46f538990
I-M-Marinov <lcfrrr@gmail.com>
2023-09-14 16:23:37 -0500

    Adding P5 to P9
    Solutions to:
    P5. Fashion Boutique
    P6. Song Queue
    P7. Truck Tour
    P8. Balanced Parenthesis
    P9. Simple Text Editor

--- P5.FashionBoutique/Program.cs
+++ P5.FashionBoutique/Program.cs
@@ -1,2 +1,25 @@
-﻿
+﻿int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
 
+int capacity = int.Parse(Console.ReadLine());
+
+Stack<int> racks = new Stack<int>(input);    
+
+int totalRacksUsed = 1;
+int currentCapacity = 0;
+
+while (racks.Count > 0)
+{
+    int currentValue = racks.Pop();
+
+    if (currentValue + currentCapacity <= capacity)
+    {
+        currentCapacity += currentValue;
+    }
+    else
+    {
+        totalRacksUsed++;
+        currentCapacity = currentValue;
+    }
+}
+
+Console.WriteLine(totalRacksUsed);
---
+++ P7.TruckTour/Program.cs
@@ -0,0 +1,42 @@
+﻿
+
+int petrolPumpCount = int.Parse(Console.ReadLine());
+
+Queue<int[]> pumps = new Queue<int[]>();
+
+for (int i = 0; i < petrolPumpCount; i++)
+{
+    int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+    pumps.Enqueue(input);
+}
+
+int bestRoute = 0;
+while (true)
+{
+    int totalPetrol = 0;
+
+    foreach (int[] pump in pumps)
+    {
+        totalPetrol += pump[0];
+        int currentDistance = pump[1];
+
+        if (totalPetrol - currentDistance < 0)
+        {
+            totalPetrol = 0;
+            break;
+        }
+        else
+        {
+            totalPetrol -= currentDistance;
+        }
+    }
+    if (totalPetrol > 0)
+    {
+        break;
+    }
+    bestRoute++;
+    pumps.Enqueue(pumps.Dequeue());
+} 
+
+Console.WriteLine(bestRoute);
+
